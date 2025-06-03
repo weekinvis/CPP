6f0bf5a8409471ab73dc3e1a01f7dbb896d84bf4
weekinvis <89652278+weekinvis@users.noreply.github.com>
2025-06-02 21:48:03 -0300

    Add files via upload

---
+++ netstudent/comparacoes.hpp
@@ -0,0 +1,10 @@
+#pragma once
+#ifndef bdop_comparacoes_h__
+#define bdop_comparacoes_h__
+
+bool comparaPorAulasDadas(const materia& a, const materia& b);
+bool comparaNotasFinais(const materia& a, const materia& b);
+bool comparaPorFaltas(const materia& a, const materia& b);
+bool comparaPorNome(const materia& a, const materia& b);
+
+#endif
